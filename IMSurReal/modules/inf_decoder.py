d7f92a16b5099148add681d63a50660c4f373baf
Xiang Yu <eggplant.elf@gmail.com>
2020-10-02 10:31:47 +0200

    revert the previous fix, cancel check for now

--- IMSurReal/modules/inf_decoder.py
+++ IMSurReal/modules/inf_decoder.py
@@ -37,8 +37,8 @@ class InfDecoder(Decoder):
 
         self.c2i = c2i
         self.emb = emb
-        self.i2e = ['<=>', '</$>', '<✓>', '<✗>'] + [c for c in c2i.keys()]
-        assert len(self.i2e) == len(set(self.i2e))
+        self.i2e = ['<=>', '</$>', '✓', '✗'] + [c for c in c2i.keys()]
+#        assert len(self.i2e) == len(set(self.i2e))
         self.e2i = {e: i for i, e in enumerate(self.i2e)}
         self.max_len = 100
 
